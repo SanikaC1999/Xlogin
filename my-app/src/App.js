1754d88288f22fe24625dcfe65088640d94834c8
SanikaC1999 <131801208+SanikaC1999@users.noreply.github.com>
2024-03-02 16:57:52 +0530

    error7

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,69 +4,53 @@ import "./App.css";
 export default function App() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const passwordInputRef = useRef(null);
-  const usernameInputRef = useRef(null);
-  const [message , setMessage] = useState("")
-  const [errorMessage, setErrorMessage] = useState("");
-  const [authenticated, setAuthenticated] = useState(false);
+  const [error, setError] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false)
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
-      if (username === "") {
-        alert("Please fill the Username field.");
-        usernameInputRef.current.focus();
-      } else if (password === "") {
-        alert("Please fill the Password field.");
-        passwordInputRef.current.focus();
-      }
-
     if (username === "user" && password === "password") {
-      setMessage("Welcome, user")
-      alert("Welcome, user");
-      setErrorMessage("");
-      setAuthenticated(true);
+      setError("")
+      setIsSubmitted(true)
     } else {
-      setMessage("Invalid username or password")
-      alert("Invalid username or password");
-      setUsername("")
-      setPassword("")
-      setErrorMessage("Invalid username or password");
-      setAuthenticated(false);
-      setTimeout(() => {
-        setErrorMessage("");
-      }, 3000);
+      setError("Invalid username or password")
+      setIsSubmitted(false)
     }
   };
 
   return (
     <div className="App">
       <h1>Login Page</h1>
-      <p>{message}</p>
-      <form className="login-form" onSubmit={handleSubmit}>
-        <label>
-          Username:
+      {
+        isSubmitted ? (<div>
+          <p>Welcome, {username}</p>
+          </div>) :
+        (<form className="login-form" onSubmit={handleSubmit}>
+          {error && <p>{error}</p>}
+          <div>
+        <label htmlFor="username">Username:</label>
           <input
-            id="usernameInput"
+            id="username"
             type="text"
             value={username}
+            placeholder="Username"
             onChange={(e) => setUsername(e.target.value)}
             required
           />
-        </label>
-        <label>
-          Password:
+          </div>
+          <div>
+        <label htmlFor="password">Password:</label>
           <input
-            ref={passwordInputRef}
-            id="passwordInput"
+            id="password"
             type="password"
             value={password}
+            placeholder="Password"
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-        </label>
+          </div>
         <input type="submit" value="Login" />
-      </form>
+      </form>)}
     </div>
   );
 }
